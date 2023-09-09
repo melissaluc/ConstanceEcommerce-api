65177c72fb68765c693ec5d9330d4586f1453f45
Melissa <66793695+MissmelissaL@users.noreply.github.com>
2023-09-09 10:08:14 -0400

    change host

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,8 @@ require('dotenv').config()
 module.exports = {
     client: "mysql",
     connection: {
-        host: "127.0.0.1",
+        host:process.env.DB_HOST,
+        // host: "127.0.0.1",
         port: 3306,
         user: process.env.DB_LOCAL_USER,
         password: process.env.DB_LOCAL_PASSWORD,
