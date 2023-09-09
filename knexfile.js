f15127c71833702dd5eb2d6a1f9404d83d90dbe5
Melissa <66793695+MissmelissaL@users.noreply.github.com>
2023-09-09 14:13:02 -0400

    add connection str w/ knex and env

--- knexfile.js
+++ knexfile.js
@@ -3,14 +3,15 @@ require('dotenv').config()
 
 module.exports = {
     client: "pg",
-    version: '15.0',
+    // version: '15.0',
     connection: {
-        host:process.env.DB_HOST,
-        port: process.env.PORT,
-        user: process.env.DB_LOCAL_USER,
-        password: process.env.DB_LOCAL_PASSWORD,
-        database: process.env.DB_LOCAL_DBNAME,
-        charset: "utf8"
+        // host:process.env.DB_HOST,
+        // port: process.env.PORT,
+        // user: process.env.DB_LOCAL_USER,
+        // password: process.env.DB_LOCAL_PASSWORD,
+        // database: process.env.DB_LOCAL_DBNAME,
+        // charset: "utf8"
+        connectionString: process.env.DB_URL
 
     }
 };
