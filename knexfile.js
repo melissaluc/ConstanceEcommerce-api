f57561870d34a928aa6e5990eb263753f746feaf
Melissa <66793695+MissmelissaL@users.noreply.github.com>
2023-09-09 13:42:40 -0400

    updated config and env again to attempt to connect to render.com

--- knexfile.js
+++ knexfile.js
@@ -5,9 +5,8 @@ module.exports = {
     client: "pg",
     version: '15.0',
     connection: {
-        // host:process.env.DB_HOST,
-        server:process.env.DB_SERVER,
-        port: 3306,
+        host:process.env.DB_HOST,
+        port: env.PORT,
         user: process.env.DB_LOCAL_USER,
         password: process.env.DB_LOCAL_PASSWORD,
         database: process.env.DB_LOCAL_DBNAME,
